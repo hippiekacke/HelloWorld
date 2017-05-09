a3d98603af216ef370a36bfe69cf8961e7e72280
bosshard <amarkus.meier@gmail.com>
2017-05-09 21:46:34 +0200

    cvyfv

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,35 @@
-
 public class HelloWorld {
 
+	private int nummer;
+
 	public static void main(String[] args) {
+		
+		int testwert = 5;
+		
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		
+		if (testwert < 8) {
+			System.out.println(true);
+		}
+		else {
+			System.out.println(false);
+		}
+			
+		
 
+	
+
+	}
+
+	private boolean test(int nummer) {
+
+		if (nummer > 5)
+			return false;
+
+		else
+			return true;
 	}
 
 }
